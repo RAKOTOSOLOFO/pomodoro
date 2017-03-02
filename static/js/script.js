b226b8d8eadd512b090b4137d8ea01364a3c801e
bl00m <leon.pierre@openmailbox.org>
2017-03-02 14:44:56 +0100

    début js ajout de tache dans todo

---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    console.log('hello');
+
+    $('#addtask').click(function() {
+        var task = $('#input').val();
+        $('#input').val('');
+
+        var span = $('<span>').attr('class', 'glyphicon glyphicon-remove-sign');
+        span.click(function() {
+            if ($(this).parent().attr('id') != 'selectedtask') {
+                $(this).parent().remove();
+            }
+        });
+
+        var li = $('<li>');
+        li.click(function() {
+            var taskS = $(this).find('p').html();
+            $("#curnttask").html(taskS);
+            $('#selectedtask').attr('id', '');
+            $(this).attr('id', 'selectedtask');
+        });
+
+        li.append(span);
+        li.append($('<p>').html(task));
+
+        $('#todo').append(li);
+    });
+});
