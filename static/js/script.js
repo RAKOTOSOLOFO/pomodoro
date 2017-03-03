5e60d84ea314a3d6d56b114ca840d681a11d49cf
bl00m <leon.pierre@openmailbox.org>
2017-03-03 14:38:01 +0100

    quit before time & note time spent

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-
 // Fonction qui convertit un nombre entier (nb) en chaine de 2 charactère.
 function dchiffre(nb) {
 	if(nb < 10) {// si le chiffre indiqué est inférieurs à dix ...
@@ -8,6 +7,12 @@ function dchiffre(nb) {
 	return nb;
 }
 
+function getTime(sec) {
+    var min = Math.floor(sec/60);
+    var second = sec%60;
+    return min + 'min ' + second + 'sec';
+}
+
 // Fonction qui démare le chronomètre
 function launchInterval(m, s) {
     var temps = setInterval(function() { // setInterval défini une fonction qui s'éxécutera toute les x miliseconde
@@ -15,8 +20,6 @@ function launchInterval(m, s) {
             clearInterval(temps); // On suprime l'interval. Termine le chronometre
             bo = true;
 
-            // $("#s").html('00'); // On passe le chronometre à zero
-            // $("#m").html('00');
             $('#m').val('0');
             $('#s').val('0');
             $('#m, #s').prop('disabled', false);
@@ -32,11 +35,10 @@ function launchInterval(m, s) {
                 s=59; // et on passe à 59 secondes
             }
 
-            // $("#s").html(dchiffre(s)); // On actualise les minutes et seconde du cadran avec m et s
-            // $("#m").html(dchiffre(m));
-            $('#m').val(m);
+            $('#m').val(m); // On actualise les minutes et seconde du cadran avec m et s
             $('#s').val(s);
         }
+        secondCounter++;
 
     }, 1000); // Intervalle de répétition en miliseconde
 
@@ -49,14 +51,26 @@ var temps; // contient l'interval du chronomettre
 var bo = true; // permet de savoir si l'interval est lancé. S'il est lancé alors bo = false et sinon bo = true (permet de ne pas lancer l'interval plusieurs fois)
 var paused = false; // On défini pause à false (deviendra true que si on clique sur "pause")
 
+var secondCounter = 0;
+
+
 $(document).ready(function() {
 
-    $('#m, #s').prop('disabled', false);
-    $('#m').val('25');
-    $('#s').val('0');
+    $('#m, #s').prop('disabled', false); // On active les input du chrono (pour pouvoir modifier)
+    var minute = 25;
+    var second = 0;
+    $('#m').val(minute);
+    $('#s').val(second);
 
+    $('#m').on('input', function() { // Quand on modifie le champs minute, on mets sa valeur dans la variable minute
+        minute = $(this).val();
+    });
+    $('#s').on('input', function() {
+        second = $(this).val();
+    });
 
     $('#addtask').click(function(event) { // On ajoute un événement click sur le bouton qui a l'id 'addtask'
+        console.log('testy');
         event.preventDefault(); // Eviter que le formulaire soit envoyé (et donc que la page recharge)
         var task = $('#input').val(); // Récupère la valeur du champs à l'id input
         if (task != '') { // Vérifier que le nom de la tache a été saisi
@@ -93,13 +107,9 @@ $(document).ready(function() {
             if ($('#selectedtask').length > 0) { // Vérifie qu'il y'a plus de 0 élément qui ont l'id "selectedtask"
                 m = parseInt($('#m').val());
                 s = parseInt($('#s').val())
-                // $('#m').html(dchiffre(m));
-                // $('#s').html(dchiffre(s));
-                // $('#m').val('25');
-                // $('#s').val('0');
                 temps = launchInterval(m, s); // On lance l'interval (voir fonction en haut)
             }
-            $('#m, #s').prop('disabled', true);
+            $('#m, #s').prop('disabled', true); // On desactive les input du chrono
         }
 
     });
@@ -112,7 +122,7 @@ $(document).ready(function() {
             bo = true // On passe bo a true (puisqu'on stope l'intervalle)
             paused = true; // On pase pause a true (puisqu'on passe en pause)
             $(this).html('Play'); // On change le contenu du boutton par "Play"
-            $('#m, #s').prop('disabled', false);
+            $('#m, #s').prop('disabled', false); // On active les input du chrono (pour pouvoir modifier)
 
         } else if (bo && paused) { // SI l'interval n'est pas lancé et que pause est true
             m = parseInt($('#m').val()); // On récupère les minutes et les seconde dans l'html
@@ -120,7 +130,7 @@ $(document).ready(function() {
             temps = launchInterval(m, s); // On relance l'intervalle
             paused = false; // Et on mets pause a false (puisq'on sort de la pause)
             $(this).html('Pause'); // On rechange le contenu du bouton par "Pause"
-            $('#m, #s').prop('disabled', true);
+            $('#m, #s').prop('disabled', true);  // On desactive les input du chrono
         }
     });
 
@@ -128,16 +138,21 @@ $(document).ready(function() {
         var task = $('#selectedtask'); // On récupère la tache
         task.detach(); // On la retire du html
         task.attr('id', ''); // Change son id (pour enlever l'id 'selectedtask')
+        task.find('p').html(task.find('p').html() + ' - ' + getTime(secondCounter));
+        secondCounter = 0;
         $('#done').append(task); // On l'ajoute à la liste 'Fait'
 
         task.unbind(); // Suprime les event associé à task.
 
         $("#curnttask").html(''); // On efface le nom de la tache courante
 
-        // $('#m').html('25');
-        // $('#s').html('00');
-        $('#m').val('25');
-        $('#s').val('0');
+        $('#m').val(minute);
+        $('#s').val(second);
+
+        if (!bo) {
+            clearInterval(temps); // On stop l'intervalle lancer
+            bo = true;
+        }
 
 
         $('.endbtn').hide(); // Cacher les boutons 'endbtn'
@@ -145,7 +160,10 @@ $(document).ready(function() {
     });
 
     $('#recommencer').click(function() {
+        $('#m').val(minute);
+        $('#s').val(second);
         $('#start').trigger( "click" ); // Déclancher l'évenement click associé à l'élément qui a l'id 'start'
+
         $('.endbtn').hide(); // Cacher et faire réaparaitre les boutons
         $('.timerbtn').show();
     });
